bd5737ed29a733a7b64d7180d4784af0b7136cdf
stellapurifi <85525657+stellapurifi@users.noreply.github.com>
2022-01-18 16:09:51 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+* Hello.java
+***
+* Stella Purificacion
+* 1 18 2022
+* CMSC 255
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Stella Purificacion");
+		System.out.println("Hello.java");
+		System.out.println("This program will display my name.");		
+		System.out.println("1 18 2022");		
+		System.out.println("CMSC 255");		
+		System.out.println();		
+	}
+}
